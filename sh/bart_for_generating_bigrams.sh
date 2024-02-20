f2c31b51e37bda331121fe74b43b1991d2ec671f
tomyoung903 <tomyoung903@gmail.com>
2024-02-20 06:25:48 +0000

    .

--- sh/bart_for_generating_bigrams.sh
+++
@@ -1,4 +0,0 @@
-nohup srun -p gpu-a100 -n 1 -t 20:00:00 python bart_for_generating_bigrams_for_bert.py \
---save_file './data/pkls/acceptable_alternatives_bigrams.pkl' \
---no_stories 1000 \
-> nohups/nohup_bart_bigrams.out &
